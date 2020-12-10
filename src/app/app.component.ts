5809169c61c144e75c2ccec55d35f21450aad0bd
Fabio C <fabio.c.colaco@gmail.com>
2020-12-10 10:28:04 +0100

    complete Auth and authGuard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyBLxBFukj4DKKE8uiC2aYUvVG1G9ZUDy8A",
+      authDomain: "bookshelves-uq.firebaseapp.com",
+      projectId: "bookshelves-uq",
+      storageBucket: "bookshelves-uq.appspot.com",
+      messagingSenderId: "1037503422080",
+      appId: "1:1037503422080:web:09a3b44876c10ded675065"
+    };
+    firebase.default.initializeApp(firebaseConfig);
+  }
 }
